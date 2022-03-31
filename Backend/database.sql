6c6e61b4b0ada2b6d1107335175f0288b7fefb92
Dcouch97 <dcouch1997@gmail.com>
2022-03-30 23:15:23 -0400

    fixed missing ;'s and added fake data

--- Backend/database.sql
+++ Backend/database.sql
@@ -4,7 +4,7 @@ CREATE TABLE Admin{
     email varchar(60) NOT NULL,
     password varchar(12) NOT NULL,
     PRIMARY KEY(admin_id)
-}
+};
 
 CREATE TABLE Contractor_User{
     con_user_id int NOT NULL,
@@ -12,7 +12,7 @@ CREATE TABLE Contractor_User{
     email varchar(60) NOT NULL,
     password varchar(12),
     PRIMARY KEY(con_user_id)
-}
+};
 
 CREATE TABLE Contractor{
     con_id int NOT NULL, 
@@ -20,7 +20,7 @@ CREATE TABLE Contractor{
     con_name varchar(80) NOT NULL,
     PRIMARY KEY(con_id),
     FOREIGN KEY(con_user_id) REFERENCES Contractor_User(con_user_id)
-}
+};
 
 CREATE TABLE Mil_Personel{
     mil_id int PRIMARY KEY,
@@ -29,7 +29,7 @@ CREATE TABLE Mil_Personel{
     email varchar(60) NOT NULL,
     password varchar(12) NOT NULL,
     PRIMARY KEY(mil_id)
-}
+};
 
 CREATE TABLE MIPR{
     MIPR_id int NOT NULL,
@@ -37,7 +37,7 @@ CREATE TABLE MIPR{
     MIPR_name varchar(80),
     PRIMARY KEY(MIPR_id),
     FOREIGN KEY(mil_id) REFERENCES Mil_Personel(mil_id)
-}
+};
 
 CREATE TABLE Project{
     project_id int NOT NULL UNIQUE,
@@ -56,7 +56,7 @@ CREATE TABLE Project{
     FOREIGN KEY(con_id) REFERENCES Contractor(con_id),
     FOREIGN KEY(mil_id) REFERENCES Mil_Personel(mil_id),
     FOREIGN KEY(MIPR_id) REFERENCES MIPR(MIPR_id)
-}
+};
 
 CREATE TABLE Messages{
     mess_id int NOT NULL,
@@ -70,7 +70,7 @@ CREATE TABLE Messages{
     FOREIGN KEY(project_id) REFERENCES Project(project_id),
     FOREIGN KEY(mil_id) REFERENCES Project(mil_id),
     FOREIGN KEY(con_user_id) REFERENCES Contractor(con_user_id)
-}
+};
 
 -- Need looking at since 2 FK's Need to find a way to make it the main key
 CREATE TABLE Contract_Award{
@@ -86,7 +86,7 @@ CREATE TABLE Contract_Award{
     awarded DATE NOT NULL,
     FOREIGN KEY(contract_num) REFERENCES Project(contract_num),
     FOREIGN KEY(contract_status) REFERENCES Project(contract_status)
-}
+};
 
 CREATE TABLE Funding{
     funding_doc_num int NOT NULL, 
@@ -100,7 +100,7 @@ CREATE TABLE Funding{
     projected_oblig_plan_year varchar(20),
     PRIMARY KEY(funding_doc_num),
     FOREIGN KEY(project_id) REFERENCES Project(project_id)
-}
+};
 
 CREATE TABLE CLIN{
     CLIN_num int NOT NULL, 
@@ -108,5 +108,41 @@ CREATE TABLE CLIN{
     CLIN_scope varchar(50) NOT NULL, 
     projected_CLIN_value money NOT NULL,
     PRIMARY KEY(CLIN_num)
-}
+};
 
+-- Fake Data
+INSERT INTO Admin(admin_id, admin_user_name, email, password) 
+VALUES(01, "admin", "admin@email.com", 'password');
+
+INSERT INTO Contractor_User(con_user_id, con_user_name, email, password)
+VALUES(01, "test_con_user", "testcon@email.com", 'password123');
+
+INSERT INTO Contractor(con_id, con_user_id, con_name)
+VALUES(01, 01, "test_con");
+
+INSERT INTO Mil_Personel(mil_id, mil_user_name, job_title, email, password)
+VALUES(01, "test_mil_user", "Project Manager", "mil_user@milemail.us", "password");
+    
+INSERT INTO MIPR(MIPR_id, mil_id, MIPR_name)
+VALUES(01, 01, "mil_mipr");
+
+INSERT INTO Project{project_id, con_id, mil_id, MIPR_id, project_name, contract_num,
+ contract_status, branch, requirement_type, summary, ipt_member, CLIN_num)
+ VALUES(01, NULL, 01, 01, "test_project", 01, "Pre-Approved", "test_branch", 0101, "test summary", 
+        "THIS IS WRONG IM SURE", 01);
+
+INSERT INTO Messages(mess_id, project_id, mil_id, con_user_id, user_message, date_posted, time_posted)
+VALUES(01, 01, 01, NULL, "test message", 01/01/2022, 23:00:00);
+
+INSERT INTO Contract_Award(contract_num, contract_status, requirement_plan, draft_rfp_released,
+ approved_by_acb, rfp_released, proposal_received, tech_eval_comp, nego_comp, awarded)
+ VALUES(01, "Pre-Awarded", 01/01/2022, 01/02/2022, 01/03/2022, 01/04/2022, 01/05/2022,
+        01/06/2022, 01/07/2022, 01/08/2022);
+
+INSERT INTO Funding(funding_doc_num, project_id, indepen_cost_est, 
+                    projected_contract_value, approved_funding, approved_funding_type, 
+                    approved_funding_fiscal_year, projected_oblig_plan_type, projected_oblig_plan_year)
+VALUES(01, 01, $1000, $2000, $10000, "funding_type_test", "fiscal_year_test", NULL, NULL);
+
+INSERT INTO CLIN(CLIN_num, CLIN_type, CLIN_scope, projected_CLIN_value)
+VALUES(01, "clin_type_test", "clin_scope_test", $420,000,000);
