4d31c1721571926770bc1cc08ddf36388f5af2c1
Robert-Ritter <bo10pcfl@gmail.com>
2022-04-03 14:52:43 -0400

    login page

--- usaf-dashboard/src/pages/Login.js
+++ usaf-dashboard/src/pages/Login.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import './home.css';
 function Login() {
     return (
-        <div>
-            <h1>Login Page</h1>
+        <body class="lightBlue">
+            <br />
+            <form class="login">
+                <label class="loginLabel" for="email">Email:</label>
+
+                    <input
+                        class="loginInput"
+                        type="text"
+                        name="email"
+                        id="email"
+
+                    />
+                <br />
+                <label class="loginLabel" for="password">Password:</label>
+                    <input
+                        class="loginInput"
+                        type="password"
+                        name="password"
+                        id="password"
+                    />
+
+                <input
+                    type="submit"
+                    value="Login"   
+                />
+            </form>
+
+            <br />
+
             <span>
                 Don't have an account? 
                 <Link to="/signup"> Sign up</Link>
             </span>
             <br />
             <span>Go back <Link to="/">Home</Link></span>
-        </ div>
+        </ body>
     );
 }
 
