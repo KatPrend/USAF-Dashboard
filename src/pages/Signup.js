6232df8a05c51f61e33d4f051800ba5113ee6504
CooperUrbec <49803257+CooperUrbec@users.noreply.github.com>
2022-04-12 13:19:13 -0400

    finished signup page ui

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,17 +1,63 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './home.css';
 
 function Signup() {
+
+
+
     return (
-        <div>
-            <h1>Signup Page</h1>
-            <span>
-                Already have an account? 
-                <Link to="/login"> Login</Link>
-            </span>
+        <body class="lightBlue">
             <br />
-            <span>Go back <Link to="/">Home</Link></span>
-        </div>
+            <form class="signup">
+
+                <p class="loginHeader">
+                    <Link to="/login"> Login </Link>
+                    <span class="accent">/ Create Account</span>
+                </p>
+
+                <div class="inputWrapper">
+
+                    <label class="signupLabel" for="firstName">First Name:</label>
+                    <input class="signupInput" type="text" >
+                    </input>
+
+                    <label class="signupLabel" for="lastName">Last Name:</label>
+                    <input class="signupInput" type="text" >
+                    </input>
+
+                    <label class="signupLabel" for="email">Email:</label>
+                    <input class="signupInput" type="text" >
+                    </input>
+
+                    <p class="passwordRequirementText">
+                        Password must include:
+                        <ul>
+                            <li>a lowercase letter</li>
+                            <li>an uppercase letter</li>
+                            <li>a number</li>
+                            <li>a special character</li>
+                        </ul>
+                    </p>
+
+                    <label class="signupLabel" for="password">Password:</label>
+                    <input class="signupInput" type="text" >
+                    </input>
+
+                    <label class="signupLabel" for="password">Confirm Password:</label>
+                    <input class="signupInput" type="text" >
+                    </input>
+
+                    <input type="submit" value="Create Account">
+
+                    </input>
+
+                </div>
+            </form>
+
+
+
+        </body>
     );
 }
 
