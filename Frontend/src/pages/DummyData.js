daf2ae1a4d8381fe11d9efcb48377a4dff2f8dbf
CooperUrbec <49803257+CooperUrbec@users.noreply.github.com>
2022-10-05 14:50:23 -0400

    starting on charts

---
+++ Frontend/src/pages/DummyData.js
@@ -0,0 +1,38 @@
+export const FinanciaData = [
+    {
+        "name": "Jan22",
+        "expected": 1000,
+        "actual": 900
+    },
+
+    {
+        "name": "Feb22",
+        "expected": 10000,
+        "actual": 9000
+    },
+
+    {
+        "name": "Mar22",
+        "expected": 500,
+        "actual": 600
+    },
+
+    {
+        "name": "Apr22",
+        "expected": 5000,
+        "actual": 1000
+    },
+
+    {
+        "name": "May22",
+        "expected": 1000,
+        "actual": 5000
+    },
+
+    {
+        "name": "Jun22",
+        "expected": 1000,
+        "actual": 900
+    },
+
+]
