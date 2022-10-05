3238c67926037217f2be9bec0529906b0e7884f7
tanflem <tanner.fleming@gmail.com>
2022-10-05 13:04:43 -0400

    syntax

--- Backend/backend_index.js
+++ Backend/backend_index.js
@@ -1,9 +1,10 @@
 const { endpoint, port, host, user, password, db_port, db_name } = require('../Backend/config');
 const express = require('express');
 const fs = require('fs');
-const mysql = require('mysql')
-const app = express()
+const mysql = require('mysql');
+const app = express();
 const cors = require('cors');
+
 app.use(cors({
   origin: 'http://localhost:3000'
 }));
@@ -24,11 +25,11 @@ db.connect(function (err) {
       return console.error('error: ' + err.message)
     }
     console.log('Database connected.')
-  })
+  });
 
 app.get('/', (req, res) => {
      console.log("This works");
- })
+ });
 
 app.post("/newProject", (req, res) => {
   const {email, password} = req.body;
@@ -53,8 +54,8 @@ app.get('/api/getproject', (req, res) => {
           throw err
       }
       res.send(results)
-  })
-})
+  });
+});
 
 app.get('/api/getprojectbyuser/:userEmail', (req, res) => {
   let sql = 'SELECT * FROM `users` u inner join user_project_link upl on upl.user_id = u.user_id inner join project p on p.project_id = upl.project_id WHERE u.userEmail =' + '"' + req.params.userEmail + '"';
@@ -63,11 +64,11 @@ app.get('/api/getprojectbyuser/:userEmail', (req, res) => {
           throw err
       }
       res.send(results)
-  })
-})
+  });
+});
   
 let nodeServer = app.listen(port, function () {
   let port = nodeServer.address().port
   let host = nodeServer.address().address
   console.log('App working on: ', host, port)
-})
+});
