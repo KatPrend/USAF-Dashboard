137e1aaa791301f99af8132fdfb8c7b0f15b17cc
CooperUrbec <49803257+CooperUrbec@users.noreply.github.com>
2022-10-12 15:35:21 -0400

    small fix

--- Frontend/src/components/FundingDataTable.jsx
+++ Frontend/src/components/FundingDataTable.jsx
@@ -1,11 +1,30 @@
 import React from "react";
-import { } from 'react-bootstrap';
+import { Table } from 'react-bootstrap';
 
 
 export default function FundingDataTable({data}){
 
     return(
         <div>
+            <Table responsive>
+                <tbody>
+                    <tr>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Funding Type</td>
+                    </tr>
+                    <tr>
+                        <td>Fiscal Year</td>
+                    </tr>
+                    <tr>
+                        <td>Projected</td>
+                    </tr>
+                    <tr>
+                        <td>Actual</td>
+                    </tr>
+                </tbody>
+            </Table>
         </div>
     )
 }
