699140405ede30c3cab4a0c7330ccaec9491d4ce
Kathleen Prendergast <katprendergast00@gmail.com>
2022-10-12 16:14:27 -0400

    File Upload

--- Backend/ingestExcel.js
+++ Backend/ingestExcel.js
@@ -12,33 +12,35 @@ router.use(
 const readXlsxFile = require('read-excel-file/node')
 const multer = require('multer')  
   
-  const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, __dirname + '/uploads/')
+const storage = multer.diskStorage({
+        destination: (req, file, cb) => {
+        cb(null, __dirname + '/uploads/')
     },
     filename: (req, file, cb) => {
-      cb(null, file.fieldname + '-' + Date.now() + '-' + file.originalname)
+        cb(null, file.fieldname + '-' + Date.now() + '-' + file.originalname)
     },
-  })
-  const uploadFile = multer({ storage: storage })
-  router.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-  })
-  router.post('/import-excel', uploadFile.single('import-excel'), (req, res) => {
+})
+
+const uploadFile = multer({ storage: storage })
+    router.get('/', (req, res) => {
+        res.sendFile(__dirname + '/index.html')
+})
+
+router.post('/import-excel', uploadFile.single('import-excel'), (req, res) => {
     importFileToDb(__dirname + '/uploads/' + req.file.filename)
     console.log(res)
-  })
+})
 
-  function importFileToDb(exFile) {
+function importFileToDb(exFile) {
     readXlsxFile(exFile).then((rows) => {
-      rows.shift()
-      
+        rows.shift()
+        
         let query = 'INSERT INTO project_info_import (`TASK ID`, `Task Description`,  `Month`,  `WBS`, `CLIN`, `Source Type`, `Resource`, `Resource Description`, `Resource Type`, `Rate`, `Hours`, `Units`, `Cost`, `Base Cost`, `Direct Cost`, `Total Price`) VALUES ?'
         db.query(query, [rows], (error, response) => {
         console.log(error || response)
         })
         
     })
-  }
+}
 
 exports.module = router;
--- Frontend/src/Routes.js
+++ Frontend/src/Routes.js
@@ -3,16 +3,10 @@ import { Switch, Route } from 'react-router-dom';
 import Main from './pages/Main';
 import Clin from './pages/Clin';
 import GetExample from './pages/GetExample';
-<<<<<<< Updated upstream
 import NewProject from './pages/NewProject';
 import PreAwardProject from './pages/PreAwardProject';
 import AwardedProject from './pages/AwardedProject';
-=======
-import PostExample from './pages/PostExample';
-import PreAwardProject from './pages/PreAwardProject'
-import AwardedProject from './pages/AwardedProject'
 import Dependency from './pages/Dependency';
->>>>>>> Stashed changes
 
 function Routes() {
     return (
@@ -35,13 +29,10 @@ function Routes() {
             <Route exact path="/newProject">
                 <NewProject />
             </Route>
-<<<<<<< Updated upstream
-=======
             <Route exact path="/dependency">
                 <Dependency />
             </Route>
 
->>>>>>> Stashed changes
         </Switch>
     );
 }
