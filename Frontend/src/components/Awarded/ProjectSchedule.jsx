26572d752f7d04349e1d805cbeadc789fcbb46df
CooperUrbec <49803257+CooperUrbec@users.noreply.github.com>
2022-10-20 11:18:56 -0400

    Gannt chart done

--- Frontend/src/components/Awarded/ProjectSchedule.jsx
+++ Frontend/src/components/Awarded/ProjectSchedule.jsx
@@ -1,8 +1,49 @@
 import React from 'react';
 import { Card, Container, Row, Col, Button, Table } from 'react-bootstrap';
 import {Chart} from "react-google-charts";
-import {TimeLineData} from '../../pages/DummyData'
+import {TimeLineData, TimeLineData2} from '../../pages/DummyData'
 
+const columns = [
+    { type: "string", label: "Task ID" },
+    { type: "string", label: "Task Name" },
+    { type: "date", label: "Start Date" },
+    { type: "date", label: "End Date" },
+    { type: "number", label: "Duration" },
+    { type: "number", label: "Percent Complete" },
+    { type: "string", label: "Dependencies" },
+  ];
+
+  function GanttChartDataFormat(JsonData){
+
+    var Rows = [];
+
+    JsonData.map( (data) => (
+        Rows.push([
+            data.ID, 
+            data.Name, 
+            new Date(data.Start), 
+            new Date(data.End), 
+            data.Duration, 
+            null,
+            data.Predecessors
+        ])
+    ))
+
+    const data = [columns, ...Rows];
+
+    return (data);
+  }
+
+  const options = {
+    gantt: {
+      criticalPathEnabled: true,
+      criticalPathStyle: {
+        stroke: "#e64a19",
+      },
+    },
+  };
+
+  
 export const ProjectSchedule = () => {
     return (
         <Card className="card">
@@ -22,30 +63,30 @@ export const ProjectSchedule = () => {
                 <Container>
                     <Row>
                         <Col>
-                            <Table>
+                            <Table responsive striped bordered hover className="bg-light">
                                 <thead>
                                     <tr>
                                         <th>ID</th>
                                         <th>Name</th>
                                         <th>Duration</th>
                                         <th>Start</th>
-                                        <th>Finish</th>
+                                        <th>End</th>
                                         <th>Predecessors</th>
                                         <th>WBS</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    {TimeLineData.map( (data) => (
+                                    {TimeLineData2.map( (data) => (
                                         <tr>
                                             <td>{data.ID}</td>
                                             <td>{data.Name}</td>
                                             <td>{data.Duration}</td>
                                             <td>{data.Start}</td>
-                                            <td>{data.Finish}</td>
+                                            <td>{data.End}</td>
                                             <td>{data.Predecessors}</td>
                                             <td>{data.WBS}</td>
                                         </tr>
-                                        
+
                                     ))}
                                 </tbody>
                             </Table>
@@ -53,10 +94,13 @@ export const ProjectSchedule = () => {
                     </Row>
                     <Row>
                         <Col>
-                            {/*<Chart
+                            <Chart
                             chartType='Gantt'
-                            data={data}
-                                    />*/}
+                            width="100%" 
+                            height="50%"
+                            options={options}
+                            data={GanttChartDataFormat(TimeLineData2)}
+                            />
                         </Col>
                     </Row>
                 </Container>
