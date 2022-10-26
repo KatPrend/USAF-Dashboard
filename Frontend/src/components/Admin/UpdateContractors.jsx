a206fa442bcb3918d7aa46d68a7f30ac9c2402f5
Kathleen Prendergast <katprendergast00@gmail.com>
2022-10-26 18:09:56 -0400

    add edit forms to admin page

--- Frontend/src/components/Admin/UpdateContractors.jsx
+++ Frontend/src/components/Admin/UpdateContractors.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
-import { Button, Card, Col, Dropdown, DropdownButton, Form, Row } from 'react-bootstrap';
+import { Button, Card, Col, Container, Dropdown, DropdownButton, Form, Row } from 'react-bootstrap';
+import "./admin.css";
 
 export const UpdateContractors = () => {
 
-    const [addContractor, setAddContractor] = useState(false);
-    const [removeContractor, setRemoveContractor] = useState(false);
     const [isLoading, setLoading] = useState(true);
     const [data, setData] = useState();
     const [contractorToRemove, setContractorToRemove] = useState();
@@ -23,28 +22,16 @@ export const UpdateContractors = () => {
         return <div className="mx-auto w-100">Loading...</div>;
     }
 
-    function chooseAdd() {
-        setAddContractor(true);
-        setRemoveContractor(false);
-    }    
-
-    function chooseRemove() {
-        setRemoveContractor(true);
-        setAddContractor(false);
-    }
-
     let handleDropdownSelect = (e) => {
         setContractorToRemove(e);
         console.log(e);
     }
 
     let handleRemove = async () => {
-        
+
         axios.delete(`/api/contractor/${contractorToRemove}`, {
         })
         .then(function(res){
-            setRemoveContractor(false);
-
             axios.get('/api/contractor/').then(response => {
                 setData(response.data);
                 setLoading(false);
@@ -73,8 +60,6 @@ export const UpdateContractors = () => {
         .then(function(res){
             // res.data.insertId
 
-            setAddContractor(false);
-
             axios.get('/api/contractor/').then(response => {
                 setData(response.data);
                 setLoading(false);
@@ -87,34 +72,39 @@ export const UpdateContractors = () => {
 
     return (
         <div>
-            <h4>Update Contractors</h4>
-            <Button className='submit-new-project' onClick={chooseAdd}>Add</Button>
-            <Button className='submit-new-project' onClick={chooseRemove}>Remove</Button>
-            {addContractor ? <>
-                <Form>
-                    <Form.Group>
-                        <Form.Label>Contractor:</Form.Label>
-                        <Form.Control type="contractor" placeholder='Contractor' onChange={handleNewContractor} />
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Label>Summary</Form.Label>
-                        <Form.Control as="textarea" placeholder="Enter Summary" type="summary" onChange={handleSummary} />
-                    </Form.Group>
-                </Form>
-                <Button className='submit-new-project' onClick={handleAdd}>Submit</Button>
-            </> : null}
-            {removeContractor ? 
-            <>
-                <DropdownButton className='dropdown' title="Contractors">
-                    {data.map(({id, contractor_name, summary}) => (
-                        <Dropdown.Item key={id} eventKey={id} onSelect={handleDropdownSelect}>
-                            {contractor_name}
-                        </Dropdown.Item>
-                    ))
-                    }
-                </DropdownButton> 
-                <Button className='submit-new-project' onClick={handleRemove}>Remove</Button>
-            </> : null}
+            <Container>
+                <Row><h4>Update Contractors</h4></Row>
+                <Row>
+                    <h5 style={{marginTop:"3%"}}>Add Contractor</h5>
+                    <Form>
+                        <Form.Group>
+                            <Form.Label>Contractor:</Form.Label>
+                            <Form.Control type="contractor" placeholder='Contractor' onChange={handleNewContractor} />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Summary</Form.Label>
+                            <Form.Control as="textarea" placeholder="Enter Summary" type="summary" onChange={handleSummary} />
+                        </Form.Group>
+                    </Form>
+                    <Button className='submit-new-project admin mx-auto' onClick={handleAdd}>Submit</Button>
+                </Row>
+                <Row>
+                    <h5>Remove Contractor</h5>
+                    <Col>
+                        <DropdownButton style={{marginTop:"2%"}} className='dropdown' title="Contractors">
+                            {data.map(({id, contractor_name, summary}) => (
+                                <Dropdown.Item key={id} eventKey={id} onSelect={handleDropdownSelect}>
+                                    {contractor_name}
+                                </Dropdown.Item>
+                            ))
+                            }
+                        </DropdownButton> 
+                    </Col>
+                    <Col>
+                        <Button className='submit-new-project admin remove' onClick={handleRemove}>Remove</Button>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
