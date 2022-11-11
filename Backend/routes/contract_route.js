0a8c7c54f8d49a4cc9d05882ddd6647cfe4838cc
Daniel Couch <dcouch1997@gmail.com>
2022-11-10 22:44:49 -0500

    Modified put for contract_award

--- Backend/routes/contract_route.js
+++ Backend/routes/contract_route.js
@@ -112,11 +112,14 @@ router.post('/contractTimeline', (req, res) => {
 // Updating a Contract Status
 router.put("/:contractid", (req, res)=>{
     
-    const {contract_status} = req.body;
+    const {contract_num, contract_status, contract_value} = req.body;
 
     let sql = `
     UPDATE contract_award 
-    SET contract_status = ${contract_status}
+    SET 
+        contract_num = "${contract_num}",
+        contract_status = "${contract_status}",
+        contract_value = "${contract_value}"
     WHERE id = ${req.params.contractid}`;
     
     db.query = db.query(sql, (err, results) =>{
