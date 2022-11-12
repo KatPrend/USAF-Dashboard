a8405f1b2bbd87a297a7e9c26882bf0a2aed1c69
Robert-Ritter <bo10pcfl@gmail.com>
2022-11-12 15:33:29 -0500

    Added Updating financial breakpoints

--- Frontend/src/pages/Admin.js
+++ Frontend/src/pages/Admin.js
@@ -5,6 +5,7 @@ import { NavB } from '../components/NavB';
 import { UpdateContractors } from '../components/Admin/UpdateContractors';
 import '../components/Admin/admin.css';
 import { UpdateFunding } from '../components/Admin/UpdateFunding';
+import { UpdateFinancialBreakpoints } from '../components/Admin/UpdateFinancialBreakpoints';
 import { UpdateBranches } from '../components/Admin/UpdateBranches';
 import { UpdateTitles } from '../components/Admin/UpdateTitles';
 import { UpdateUsers } from '../components/Admin/UpdateUsers';
@@ -13,39 +14,32 @@ import { DeleteProjects } from '../components/Admin/DeleteProjects';
 function Admin() {
     const [userid, setUserid] = useState(0);
     const [userRole, setUserRole] = useState("");
-    const [showContractor, setShowContractor] = useState(false);
-    const [showFunding, setShowFunding] = useState(false);
-    const [showBranch, setShowBranch] = useState(false);
-    const [showTitle, setShowTitle] = useState(false);
-    const [showUsers, setShowUsers] = useState(false);
-    const [showProjects, setShowProjects] = useState(false);
+    const [selectedOption, setSelectedOption] = useState("none")
 
     const getUserInfo = (uid, urole) => {
         setUserid(uid);
         setUserRole(urole);
     }
 
-    function handleShow(button) {
-        setShowContractor(false);
-        setShowFunding(false);
-        setShowBranch(false);
-        setShowTitle(false);
-        setShowUsers(false);
-        setShowProjects(false);
+    function renderBody() {
 
-        if (button === "Contractor") {
-            setShowContractor(true);
-        } else if (button === "Funding") {
-            setShowFunding(true);
-        } else if (button === "Branch") {
-            setShowBranch(true);
-        } else if (button === "Title") {
-            setShowTitle(true);
-        } else if (button === "User") {
-            setShowUsers(true);
-        } else if (button === "Project") {
-            setShowProjects(true);
+        if (selectedOption === "Contractor") {
+            return <UpdateContractors />;
+        } else if (selectedOption === "Funding") {
+            return <UpdateFunding />;
+        } else if (selectedOption === "Financial Breakpoints") {
+            return <UpdateFinancialBreakpoints />;
+        } else if (selectedOption === "Branch") {
+            return <UpdateBranches />;
+        } else if (selectedOption === "Title") {
+            return <UpdateTitles />;
+        } else if (selectedOption === "User") {
+            return <UpdateUsers />;
+        } else if (selectedOption === "Project") {
+            return <DeleteProjects />;
         }
+        // default msg
+        return <></>
     }
     
     return (
@@ -57,22 +51,18 @@ function Admin() {
                         <Col xs={3} className="options">
                             <Row><span>Settings</span></Row>
                             <Row><ButtonGroup vertical>
-                                <Button className="submit-new-project settings" onClick={() => {handleShow("Contractor")}}>Contractors</Button>
-                                <Button className="submit-new-project settings" onClick={() => {handleShow("Funding")}}>Funding Types</Button>
-                                <Button className="submit-new-project settings" onClick={() => {handleShow("Branch")}}>Branches</Button>
-                                <Button className="submit-new-project settings" onClick={() => {handleShow("Title")}}>Military Job Titles</Button>
-                                <Button className="submit-new-project settings" onClick={() => {handleShow("User")}}>Users</Button>
-                                <Button className="submit-new-project settings" onClick={() => {handleShow("Project")}}>Projects</Button>
+                                <Button className="submit-new-project settings" onClick={() => {setSelectedOption("Contractor")}}>Contractors</Button>
+                                <Button className="submit-new-project settings" onClick={() => {setSelectedOption("Funding")}}>Funding Types</Button>
+                                <Button className="submit-new-project settings" onClick={() => {setSelectedOption("Financial Breakpoints")}}>Financial Summary Breakpoints</Button>
+                                <Button className="submit-new-project settings" onClick={() => {setSelectedOption("Branch")}}>Branches</Button>
+                                <Button className="submit-new-project settings" onClick={() => {setSelectedOption("Title")}}>Military Job Titles</Button>
+                                <Button className="submit-new-project settings" onClick={() => {setSelectedOption("User")}}>Users</Button>
+                                <Button className="submit-new-project settings" onClick={() => {setSelectedOption("Project")}}>Projects</Button>
                             </ButtonGroup></Row>
                         </Col>
                         <Col className="edits">
                             <Row><span>Edit</span></Row>
-                            <Row>{showContractor ? <UpdateContractors /> : null}</Row>
-                            <Row>{showFunding ? <UpdateFunding /> : null}</Row>
-                            <Row>{showBranch ? <UpdateBranches /> : null}</Row>
-                            <Row>{showTitle ? <UpdateTitles /> : null}</Row>
-                            <Row>{showUsers ? <UpdateUsers /> : null}</Row>
-                            <Row>{showProjects ? <DeleteProjects /> : null}</Row>
+                            <Row>{renderBody()}</Row>
                         </Col>
                     </Row>
                 </Container>
