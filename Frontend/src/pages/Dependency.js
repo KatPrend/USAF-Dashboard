6b7471606ffd4e84178f56fe6dacf30253cd26c3
Robert-Ritter <bo10pcfl@gmail.com>
2022-11-14 13:40:58 -0500

    Added Search bars for dependency page

--- Frontend/src/pages/Dependency.js
+++ Frontend/src/pages/Dependency.js
@@ -16,6 +16,15 @@ import { Redirect } from 'react-router-dom';
 const ProjectContent = (props) => {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState();
+  const [predProj, setPredProj] = useState('');
+  const [predMil, setPredMil] = useState('');
+  const [predStart, setPredStart] = useState('');
+  const [predEnd, setPredEnd] = useState('');
+  const [succProj, setSuccProj] = useState('');
+  const [succMil, setSuccMil] = useState('');
+  const [succStart, setSuccStart] = useState('');
+  const [succEnd, setSuccEnd] = useState('');
+
   console.log(props.userid);
   useEffect(() => {
       axios.get(`/api/dependency/userSuccessor/${props.userid}`).then(response => {
@@ -27,6 +36,35 @@ const ProjectContent = (props) => {
 
   if (isLoading) {
       return <div className="mx-auto w-75">Loading...</div>;
+  }
+
+  const safeToString = (input) => {
+    if (input === null)
+        return 'null';
+    if (input === undefined)
+        return 'undefined';
+    return input.toString();
+  }
+
+  const shouldDisplay = (pred_proj_name, pred_name, pred_start, pred_end, succ_proj_name, succ_name, succ_start, succ_end) => {
+    // if x does not contain the xSearch and xSearch is not empty
+    if (!(safeToString(pred_proj_name).toLowerCase().includes(predProj.toLowerCase())) && predProj !== '')
+        return false;
+    if (!(safeToString(pred_name).toLowerCase().includes(predMil.toLowerCase())) && predMil !== '')
+        return false;
+    if (!(safeToString(pred_start).toLowerCase().includes(predStart.toLowerCase())) && predStart !== '')
+        return false;
+    if (!(safeToString(pred_end).toLowerCase().includes(predEnd.toLowerCase())) && predEnd !== '')
+        return false;
+    if (!(safeToString(succ_proj_name).toLowerCase().includes(succProj.toLowerCase())) && succProj !== '')
+        return false;
+    if (!(safeToString(succ_name).toLowerCase().includes(succMil.toLowerCase())) && succMil !== '')
+        return false;
+    if (!(safeToString(succ_start).toLowerCase().includes(succStart.toLowerCase())) && succStart !== '')
+        return false;
+    if (!(safeToString(succ_end).toLowerCase().includes(succEnd.toLowerCase())) && succEnd !== '')
+        return false;
+    return true;
   }
 
   return (
@@ -45,9 +83,20 @@ const ProjectContent = (props) => {
                   </tr>
               </thead>
               <tbody>
+                  <tr>
+                    <td><input placeholder="Filter by Project" style={{width: '100%'}} type='text' onChange={function (event) {setPredProj(event.target.value)}} value={predProj}></input></td>
+                    <td><input placeholder="Filter by Milestone" style={{width: '100%'}} type='text' onChange={function (event) {setPredMil(event.target.value)}} value={predMil}></input></td>
+                    <td><input placeholder="Filter by Start" style={{width: '100%'}} type='text' onChange={function (event) {setPredStart(event.target.value)}} value={predStart}></input></td>
+                    <td><input placeholder="Filter by End" style={{width: '100%'}} type='text' onChange={function (event) {setPredEnd(event.target.value)}} value={predEnd}></input></td>
+                    <td><input placeholder="Filter by Project" style={{width: '100%'}} type='text' onChange={function (event) {setSuccProj(event.target.value)}} value={succProj}></input></td>
+                    <td><input placeholder="Filter by Milestone" style={{width: '100%'}} type='text' onChange={function (event) {setSuccMil(event.target.value)}} value={succMil}></input></td>
+                    <td><input placeholder="Filter by Start" style={{width: '100%'}} type='text' onChange={function (event) {setSuccStart(event.target.value)}} value={succStart}></input></td>
+                    <td><input placeholder="Filter by End" style={{width: '100%'}} type='text' onChange={function (event) {setSuccEnd(event.target.value)}} value={succEnd}></input></td>
+                  </tr>
               {
                   data.map(({ pred_proj_name, pred_name, pred_start, pred_end, succ_proj_name, succ_name, succ_start, succ_end }, index) => (
-                      <tr key={index}>
+                      <tr style={shouldDisplay(pred_proj_name, pred_name, format(new Date(pred_start), 'MM/dd/yyyy'), format(new Date(pred_end), 'MM/dd/yyyy'),
+                                               succ_proj_name, succ_name, format(new Date(succ_start), 'MM/dd/yyyy'), format(new Date(succ_end), 'MM/dd/yyyy')) ? {} : {}} key={index}>
                           <td>{pred_proj_name}</td>
                           <td>{pred_name}</td>
                           <td>{format(new Date(pred_start), 'MM/dd/yyyy')}</td>
